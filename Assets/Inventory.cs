750fcc89c78e0e3a4b1d4a3c9873bf43a9d4651b
Tomi Hänninen <101732080+hannisentomi@users.noreply.github.com>
2024-02-13 19:09:47 +0200

    lisätty avaimet ja niiden keräily scriptit

---
+++ Assets/Inventory.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour {
+
+	public int amountOfKeys = 0;
+    public GameObject campfireUI;
+    public static bool doorOpen = false;
+
+	void PickKey () {
+
+		amountOfKeys = amountOfKeys + 1;
+
+        if(amountOfKeys == 3)
+        {
+            Debug.Log("Ovi avattu!");
+            doorOpen = true;
+        }
+	}
+}
---
+++ Assets/Key.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Key : MonoBehaviour {
+
+//	[SerializeField] private Transform pickAudioSource;
+
+	void OnTriggerEnter(Collider other) {
+
+		Debug.Log("Collision Enter " + other.gameObject.name);
+		//if(other.gameObject.name == "FPSController")
+        //{
+			//TODO: Send a message to pick the log and update inventory
+			//other.gameObject.SendMessage("PickKey");
+		GameObject.Find("GameManager").SendMessage("PickKey");
+			//Instantiate(this.pickAudioSource);
+
+			//TODO: Delete the log game object that was picked from the scene
+			Destroy(gameObject);			
+		//}
+
+	}
+
+}
